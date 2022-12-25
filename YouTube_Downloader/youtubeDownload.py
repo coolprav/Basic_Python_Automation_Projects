529ac81c0bbdce1037b30ca6545e0a67786cadd8
PRAVEEN <20909381@student.curtin.edu.au>
2022-12-25 18:51:31 +0530

    YT downloader and PDF merger

---
+++ YouTube_Downloader/youtubeDownload.py
@@ -0,0 +1,13 @@
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title: ", yt.title)
+print("View : ", yt.views)
+print("Length of the video", yt.length)
+
+yd = yt.streams.get_highest_resolution()
+yd.download('D:\YouTube')
+
